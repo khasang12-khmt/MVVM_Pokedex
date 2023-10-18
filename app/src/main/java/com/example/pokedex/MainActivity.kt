7e27e581be04456270f448ab0ede07991ee49dd0
Sang Kha <80106348+khasang12-khmt@users.noreply.github.com>
2023-10-18 23:41:51 +0700

    search & filter menu view complete

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -15,8 +15,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.pokedex.pokemonlist.PokemonListScreen
 import com.example.pokedex.ui.theme.PokedexTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -33,7 +34,7 @@ class MainActivity : ComponentActivity() {
                 ){
                     // List Screen
                     composable("pokemon_list_screen"){
-
+                        PokemonListScreen(navController = navController)
                     }
                     // Detail Screen
                     composable("pokemon_detail_screen/{dominantColor}/{pokemonName}",
